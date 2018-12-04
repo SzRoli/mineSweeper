65bc698d9a39d37a5bb51b98c23fc74657928b5f
SzRoli <szepesiroland@gmail.hu>
2018-12-04 11:29:27 +0100

    Tiles

---
+++ Minesweeper/Minesweeper/MinesweeperForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tileGrid = new Minesweeper.Form1.TileGrid();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tileGrid
+            // 
+            this.tileGrid.Location = new System.Drawing.Point(12, 70);
+            this.tileGrid.Name = "tileGrid";
+            this.tileGrid.Size = new System.Drawing.Size(482, 292);
+            this.tileGrid.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Minesweeper.Properties.Resources.MINESWEEPER_tray;
+            this.pictureBox1.Location = new System.Drawing.Point(216, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 43);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.LoadGame);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(523, 407);
+            this.Controls.Add(this.tileGrid);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TileGrid tileGrid;
+        private System.Windows.Forms.PictureBox gameButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Minesweeper/Minesweeper/MinesweeperForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Minesweeper.Properties;
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        private Difficulty difficulty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.LoadGame(null, null);
+        }
+
+        private enum Difficulty { Hard, Medium, Easy}
+
+        private void LoadGame(object sender, EventArgs e)
+        {
+            int x, y, mines;
+            switch (this.difficulty)
+            {
+                case Difficulty.Easy:
+                    x = 9;
+                    y = 9;
+                    mines = 10;
+
+                    break;
+                case Difficulty.Medium:
+                    x = 16;
+                    y = 16;
+                    mines = 40;
+                    break;
+                case Difficulty.Hard:
+                    x = 30;
+                    y = 16;
+                    mines = 99;
+
+                    break;
+                default:
+                    throw new InvalidOperationException("Not found this difficulty");
+            }
+            this.tileGrid.LoadGrid(new Size(x, y), mines);
+            this.MaximumSize = new Size(this.tileGrid.Width + 36, this.tileGrid.Height + 98);
+            this.MinimumSize = new Size(this.tileGrid.Width + 36, this.tileGrid.Height + 98);
+        }
+
+        private class TileGrid : Panel
+        {
+            private Size gridSize;
+            private int mines;
+            private int flags;
+            private void Tile_MouseDown(object sender, MouseEventArgs e)
+            {
+
+            }
+            internal void LoadGrid(Size gridSize, int mines)
+            {
+                this.Controls.Clear();
+                this.gridSize = gridSize;
+                this.mines = mines;
+                this.flags = mines;
+                this.Size = new Size(gridSize.Width * Tile.LENGTH, gridSize.Height * Tile.LENGTH +15);
+                for(int x = 0; x < gridSize.Width; x++)
+                {
+                    for(int y = 0; y<gridSize.Height; y++)
+                    {
+                        Tile tile = new Tile(x,y);
+                        tile.MouseDown += Tile_MouseDown;
+                        this.Controls.Add(tile);
+                    }
+                }
+            }
+
+            private class Tile : PictureBox
+            {
+                internal const int LENGTH = 20;
+                internal Point GridPosition { get; }
+                internal Tile(int x, int y)
+                {
+                    this.Name = $"Tile_{x}_{y}";
+                    this.Location = new Point ( x * LENGTH, y * LENGTH );
+                    this.GridPosition = new Point(x, y);
+                    this.Size = new Size(LENGTH, LENGTH);
+                    this.Image = Resources.Tile;
+                    this.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+            }
+        }
+    }
+}
